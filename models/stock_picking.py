c8a0345adbec8815552d506eee7ef582de4a4a11
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-04-02 09:07:54 -0300

    soporte multi-hoja (remitos)

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -18,38 +18,38 @@ class Picking(models.Model):
             :param separate_pickings: Indicates if pickings should be checked independently for lot/serial numbers or not.
         """
 
-        max_prod_con_serie = 90
-        max_prod_sin_serie = 30
+        # max_prod_con_serie = 1000
+        # max_prod_sin_serie = 30
         # Verificar si el picking es de despacho
-        if self.picking_type_code == 'outgoing':
+        # if self.picking_type_code == 'outgoing':
             # Contar la cantidad de productos con y sin serie
-            productos_con_serie = 0
-            productos_sin_serie = 0
-            for move_line in self.move_line_ids_without_package:
-                if move_line.qty_done > 0:
-                    if move_line.product_id.tracking == 'serial':
-                        productos_con_serie += 1
-                    else:
-                        productos_sin_serie += 1
+            # productos_con_serie = 0
+            # productos_sin_serie = 0
+            # for move_line in self.move_line_ids_without_package:
+            #     if move_line.qty_done > 0:
+            #         if move_line.product_id.tracking == 'serial':
+            #             productos_con_serie += 1
+            #         else:
+            #             productos_sin_serie += 1
 
-            # Ajustar el límite según la cantidad de productos con y sin serie
-            if productos_con_serie > 0 and productos_sin_serie > 0:
-                # Si hay productos con y sin serie, aplicar la relación 3:1
-                limite = int((productos_con_serie * 3) + productos_sin_serie)
-                maximo = max_prod_con_serie
-                if limite > maximo:
-                    excedente = limite - maximo
-                    raise UserError(_('Sólo se pueden despachar hasta ' + str(maximo) + ' items por remito, por favor elimine ' + str(excedente) + ' items y vuelva a intentarlo'))
-            elif productos_con_serie > 0:
-                # Si todos los productos tienen serie, el límite es 90
-                if productos_con_serie > max_prod_con_serie:
-                    excedente = productos_con_serie - max_prod_con_serie
-                    raise UserError(_('Sólo se pueden despachar hasta ' + str(max_prod_con_serie) + ' items por remito, por favor elimine ' + str(excedente) + ' items y vuelva a intentarlo'))
-            else:
-                # Si no hay productos con serie, el límite es 30
-                if productos_sin_serie > max_prod_sin_serie:
-                    excedente = productos_sin_serie - max_prod_sin_serie
-                    raise UserError(_('Sólo se pueden despachar hasta ' + str(max_prod_sin_serie) + ' items por remito, por favor elimine ' + str(excedente) + ' items y vuelva a intentarlo'))
+            # # Ajustar el límite según la cantidad de productos con y sin serie
+            # if productos_con_serie > 0 and productos_sin_serie > 0:
+            #     # Si hay productos con y sin serie, aplicar la relación 3:1
+            #     limite = int((productos_con_serie * 3) + productos_sin_serie)
+            #     maximo = max_prod_con_serie
+            #     if limite > maximo:
+            #         excedente = limite - maximo
+            #         raise UserError(_('Sólo se pueden despachar hasta ' + str(maximo) + ' items por remito, por favor elimine ' + str(excedente) + ' items y vuelva a intentarlo'))
+            # elif productos_con_serie > 0:
+            #     # Si todos los productos tienen serie, el límite es 90
+            #     if productos_con_serie > max_prod_con_serie:
+            #         excedente = productos_con_serie - max_prod_con_serie
+            #         raise UserError(_('Sólo se pueden despachar hasta ' + str(max_prod_con_serie) + ' items por remito, por favor elimine ' + str(excedente) + ' items y vuelva a intentarlo'))
+            # else:
+            #     # Si no hay productos con serie, el límite es 30
+            #     if productos_sin_serie > max_prod_sin_serie:
+            #         excedente = productos_sin_serie - max_prod_sin_serie
+            #         raise UserError(_('Sólo se pueden despachar hasta ' + str(max_prod_sin_serie) + ' items por remito, por favor elimine ' + str(excedente) + ' items y vuelva a intentarlo'))
 
         
         super()._sanity_check(separate_pickings)
